7273fdffeb0c70af0794f23956da796911a26501
blazebomb <ashishkumar850601@gmail.com>
2023-12-02 19:19:58 +0530

    first commit

---
+++ contracts/game.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AdventureToken is ERC20, Ownable {
+    constructor() ERC20("ADVT", "Adventure") Ownable() {
+        _mint(msg.sender, 10000 * 10 ** decimals());
+    }
+
+    mapping(address => uint[]) public playerInventory;
+    mapping(uint => GameItem) public gameItems;
+    uint public totalItemTypes = 0;
+
+    struct GameItem {
+        string name;
+        uint typeId;
+        uint256 price;
+    }
+
+    function mintTokens(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function createGameItem(
+        string memory name,
+        uint256 price
+    ) public onlyOwner {
+        totalItemTypes += 1;
+        uint typeId = totalItemTypes;
+        gameItems[typeId] = GameItem(name, typeId, price);
+    }
+
+    function getPlayerBalance() external view returns (uint256) {
+        return balanceOf(msg.sender);
+    }
+
+    function acquireGameItem(uint typeId) external {
+        require(
+            balanceOf(msg.sender) >= gameItems[typeId].price,
+            "Insufficient funds"
+        );
+        approve(msg.sender, gameItems[typeId].price);
+        transferFrom(msg.sender, address(this), gameItems[typeId].price);
+        playerInventory[msg.sender].push(gameItems[typeId].typeId);
+    }
+
+    function transferTokens(address to, uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Insufficient funds");
+        approve(msg.sender, amount);
+        transferFrom(msg.sender, to, amount);
+    }
+
+    function burnTokens(uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Insufficient funds");
+        approve(msg.sender, amount);
+        _burn(msg.sender, amount);
+    }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,46 @@
+
+require("@nomicfoundation/hardhat-toolbox");
+
+const FORK_FUJI = false;
+const FORK_MAINNET = false;
+let forkingData = undefined;
+
+if (FORK_MAINNET) {
+  forkingData = {
+    url: "https://api.avax.network/ext/bc/C/rpcc",
+  };
+}
+if (FORK_FUJI) {
+  forkingData = {
+    url: "https://api.avax-test.network/ext/bc/C/rpc",
+  };
+}
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+ 
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      gasPrice: 225000000000,
+      chainId: !forkingData ? 43112 : undefined, //Only specify a chainId if we are not forking
+      forking: forkingData
+    },
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: [
+        "bd0db010802bf1443b4d56c2dea49fd5ac42205f53345c7ffabc6bce77a55c95"
+      ]
+    },
+    mainnet: {
+      url: 'https://api.avax.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43114,
+      accounts: [
+        "bd0db010802bf1443b4d56c2dea49fd5ac42205f53345c7ffabc6bce77a55c95"
+      ]
+    }
+  }
+}
